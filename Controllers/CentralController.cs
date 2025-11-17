845154ae11ffd177ead7e49c92325fcef8d16569
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-11-17 11:24:03 +0200

    Created CentralTable page

---
+++ Controllers/CentralController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using disease_outbreaks_detector.Data;
+using disease_outbreaks_detector.Models;
+
+namespace disease_outbreaks_detector.Controllers
+{
+	public class CentralController : Controller
+	{
+
+		private readonly ApplicationDbContext _context;
+		public CentralController(ApplicationDbContext context) => _context = context;
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var data = await _context.CaseRecords
+				.Include(c => c.CountryEntity)
+				.Include(c => c.Source)
+				.ToListAsync();
+
+				var userData = await _context.Users
+			.AsNoTracking()
+			.ToListAsync();
+
+			var model = new CentralTableViewModel
+			{
+				CaseRecords = data,
+				Users = userData
+			};
+
+			return View(model);
+		}
+
+	}
+}
---
+++ Models/CentralTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace disease_outbreaks_detector.Models
+{
+	public class CentralTableViewModel 
+	{
+		public List<CaseRecord> CaseRecords { get; set; }
+		public List<AppDbContextUser> Users { get; set; }
+	}
+}
